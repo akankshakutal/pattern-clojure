618b2678b045b2dd73b28fd554e41bf2fa013600
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2020-01-23 10:16:41 +0530

    Implemented Empty-rectangle

--- src/pattern/core.clj
+++ src/pattern/core.clj
@@ -2,15 +2,33 @@
 
 (defn repeat-symbol [times symbol] (into [] (repeat times symbol)))
 
-(defn filled-rectangle
-  [width height symbol]
+(defn rectangle
+  [width height symbol pattern]
   (loop [height height result []]
     (if (zero? height)
       result
       (recur (dec height)
-             (conj result (repeat-symbol width symbol)))
+             (conj result (pattern width symbol)))
       )
     ))
+
+(defn empty-line [times symbol]
+  (let [result (assoc (repeat-symbol times " ") 0 symbol)
+        last-index (dec (count result))]
+    (assoc result last-index symbol)
+    ))
+
+(defn filled-rectangle
+  [width height symbol]
+  (rectangle width height symbol repeat-symbol)
+  )
+
+(defn empty-rectangle
+  [width height symbol]
+  (-> (rectangle width height symbol empty-line)
+      (assoc 0 (repeat-symbol width symbol))
+      (assoc (dec height) (repeat-symbol width symbol))
+      ))
 
 (defn print-shape
   [shape printer]
--- test/pattern/core_test.clj
+++ test/pattern/core_test.clj
@@ -3,12 +3,22 @@
             [pattern.core :refer :all]))
 
 (deftest print-rectangle-test
-  (testing "Filled Rectangle"
-    (is (= [["X" "X"] ["X" "X"]] (filled-rectangle 2 2 "X"))))
+  (testing "Rectangle"
+    (is (= [["X" "X"] ["X" "X"]] (rectangle 2 2 "X" repeat-symbol))))
   )
 
 (deftest print-shape-test
   (testing "Print Shape"
-    (is (= "**\n**\n**\n**\n**" (print-shape (filled-rectangle 2 5 "*") identity))))
+    (is (= "**\n**\n**\n**\n**" (print-shape (rectangle 2 5 "*" repeat-symbol) identity))))
   )
 
+
+(deftest filled-rectangle-test
+  (testing "Filled Rectangle"
+    (is (= [["X" "X"] ["X" "X"]] (filled-rectangle 2 2 "X"))))
+  )
+
+(deftest empty-rectangle-test
+  (testing "Filled Rectangle"
+    (is (= [["*" "*" "*"] ["*" " " "*"] ["*" "*" "*"]] (empty-rectangle 3 3 "*"))))
+  )
